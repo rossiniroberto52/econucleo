1cc52c0f2cbeca61db75950bcc4fb5156c479245
Rossin135 <toquehuk@gmail.com>
2024-07-03 21:23:37 -0300

    initial

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import sqlite3
+
+app = FastAPI()
+
+def DataBase(query:str):
+    with sqlite3.connect("db.db3") as conn:
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+        return data
+
+@app.get("/")
+def root():
+    return{"200"}
+
+@app.get("/livros")
+def get_all_books():
+    query = "SELECT * FROM tb_books"
+    return DataBase(query)
